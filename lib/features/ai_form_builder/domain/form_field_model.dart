0c1a0880439d5d08e1e05a2f20a2c466e6d94d50
darahat <darahat42@gmail.com>
2025-08-21 16:32:46 +0600

    Feat: Question ID field in FormFieldModel

--- lib/features/ai_form_builder/domain/form_field_model.dart
+++ lib/features/ai_form_builder/domain/form_field_model.dart
@@ -5,27 +5,36 @@ part 'form_field_model.g.dart';
 @HiveType(typeId: 8)
 /// Form Field Model is for make a table about conversation between user and ai and question types
 class FormFieldModel {
-  /// Which Questions
+  /// Which Questions ID
   @HiveField(0)
+  final String id;
+
+  /// Which Questions
+  @HiveField(1)
   final String question;
 
   /// What kind of questions are the multiple select or yes or no
-  @HiveField(1)
+  @HiveField(2)
   final String type;
 
   /// Which are questions options
-  @HiveField(2)
+  @HiveField(3)
   final List<String>? options;
 
   /// model Constructor
-  FormFieldModel({required this.question, required this.type, this.options});
+  FormFieldModel({
+    required this.id,
+    required this.question,
+    required this.type,
+    this.options,
+  });
 }
 
 @HiveType(typeId: 9)
 /// Generated Form model.
 class AiGeneratedFormModel {
   @HiveField(0)
-  /// Title is text box title
+  /// Title is form id
   final int id;
 
   @HiveField(2)
