8d0fd3d2ff01321f1ded868b01c4fa41fce73e62
darahat <darahat42@gmail.com>
2025-08-26 16:56:35 +0600

    Fix: Form_generator modified getting hiveservice instance from parameter

--- lib/core/utils/form_generator.dart
+++ lib/core/utils/form_generator.dart
@@ -1,18 +1,17 @@
 import 'dart:convert';
 
 import 'package:ai_form_builder/core/services/hive_service.dart';
-import 'package:ai_form_builder/core/utils/logger.dart';
 import 'package:ai_form_builder/features/ai_form_builder/domain/form_field_model.dart';
+import 'package:flutter/material.dart';
 
 /// Form Generator function
 class FormGenerator {
+  /// parse json which getting from ai and save to hive db
   static Future<AiGeneratedFormModel?> parseAndSaveForm(
     ref,
     HiveService hiveService,
     String jsonString,
   ) async {
-    final logger = ref.watch(appLoggerProvider);
-
     try {
       final decodedJson = jsonDecode(jsonString) as Map<String, dynamic>;
 
@@ -24,14 +23,16 @@ class FormGenerator {
               (f) => f.containsKey('question') && f.containsKey('type'),
             )) {
           final form = AiGeneratedFormModel.fromJson(decodedJson);
-          final hiveService = ref.read(hiveServiceProvider);
+          // final hiveService = ref.read(hiveServiceProvider);
           final box = hiveService.aiGeneratedFormInfoBox;
           await box.put(form.id, form);
           return form;
         }
       }
+    } on FormatException catch (e) {
+      debugPrint('Error parsing JSON: $e');
     } catch (e) {
-      logger.error('Generated Error From Form Generator dart file $e');
+      debugPrint('Error parsing JSON: $e');
     }
     return null;
   }
